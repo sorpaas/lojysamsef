3679d43beb4db742cbe46cc67a717b026bffedc1
sorpa'as plat <sorpaas@gmail.com>
2014-10-25 21:34:53 +0800

    a basic example of a echo server

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import           Control.Applicative
-import           Snap.Core
-import           Snap.Util.FileServe
-import           Snap.Http.Server
+import System.IO(hFlush, stdout)
+import System.Environment(getArgs)
+import System.Exit(exitFailure)
+import Control.Applicative((<$>))
+import Control.Arrow((&&&))
+import Control.Monad.Tools(doWhile, doWhile_)
+import LojysambanLib(ask, readRules, end)
+import Control.Applicative
+import Snap.Core
+import Snap.Util.FileServe
+import Snap.Http.Server
+import Data.ByteString.Char8(pack)
 
 main :: IO ()
+-- main = do
+--         args <- getArgs
+--         rules <- readRules <$> case args of
+--                 [] -> doWhile "" $ \s -> (id &&& not . end) . (s ++) <$> getLine
+--                 [fp] -> readFile fp
+--                 _ -> putStrLn "Usage: lojysamban [FILEPATH]" >> exitFailure
+--         doWhile_ $ do
+--                 q <- putStr ".i " >> hFlush stdout >> getLine
+--                 maybe (return False) ((>> return True) . putStrLn) $ ask q rules
+
+
+-- main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
 site =
     ifTop (writeBS "hello world") <|>
     route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
+          , ("echo", echoHandler)
           ] <|>
     dir "static" (serveDirectory ".")
 
+answerQuestion :: String -> Maybe String
+answerQuestion q =
+  ask q $ readRules ".i la .iocikun. patfu la .ituk. .i la manam. mamta la .ituk. .i da rirni de .ijanai da patfu de .i da rirni de .ijanai da mamta de"
+
 echoHandler :: Snap ()
 echoHandler = do
-    param <- getParam "echoparam"
     maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+          (writeBS . pack) (answerQuestion "ma rirni la .ituk.")
