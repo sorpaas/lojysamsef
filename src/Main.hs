f979ce313777f4775ec9095eb33852192dcb46fc
sorpa'as plat <sorpaas@gmail.com>
2014-11-01 16:10:34 +0800

    use post request for http and cors

--- src/Main.hs
+++ src/Main.hs
@@ -2,24 +2,75 @@
 
 module Main where
 
+import Control.Applicative
+import Control.Arrow
+import Control.Monad.Tools
+
 import System.IO(hFlush, stdout)
 import System.Environment(getArgs)
 import System.Exit(exitFailure)
-import Control.Applicative((<$>))
-import Control.Arrow((&&&))
-import Control.Monad.Tools(doWhile, doWhile_)
 import LojysambanLib(ask, readRules, end)
-import Control.Applicative
-import Snap.Core
-import Snap.Util.FileServe
-import Snap.Http.Server
-import Snap.Extras.JSON(writeJSON)
-import Data.ByteString.Char8(pack)
-import Data.Text.Encoding(decodeUtf8)
 import Data.Text(unpack)
-import Data.Aeson
+import Data.Text.Encoding(decodeUtf8)
+import Data.ByteString.Char8(pack)
+
+import qualified Snap.Core as Snap
+import qualified Snap.Http.Server.Config as Snap
+import qualified Snap.Snaplet as Snap
+import qualified Snap.Util.FileServe as Snap
+import qualified Snap.CORS as CORS
+import qualified Snap.Extras.JSON as JSON
+import qualified Data.Aeson as JSON
 
 main :: IO ()
+main = Snap.serveSnaplet Snap.defaultConfig site
+
+site = Snap.makeSnaplet "app" "app" Nothing $ do
+  Snap.addRoutes [ ("/", JSON.writeJSON $ AnswerResult "me")
+                 , ("/info", JSON.writeJSON $ Info "lojysamsef")
+                 , ("/ask", askHandler)]
+  CORS.wrapCORS
+
+askHandler = Snap.method Snap.POST $ do
+    param <- Snap.getParam "question"
+    maybe (JSON.writeJSON $ AnswerError "must specify echo/param in URL")
+          (\s -> (maybe
+           (JSON.writeJSON $ AnswerError "no result")
+           (\t -> (JSON.writeJSON $ AnswerResult (show t)))
+           (answerQuestion $ (unpack . decodeUtf8) s)
+          )) param
+
+answerQuestion :: String -> Maybe String
+answerQuestion q =
+  ask q $ readRules ".i da du da .i la .ualeis. cu nelci lo cirla .i la gromit. cu nelci lo cirla .i la .uendolen. cu nelci lo lanme .i da pendo de .ijanai tu'e da nadu de .i da nelci di .i de nelci di"
+
+data Person = Person {
+  name :: String
+  } deriving (Show)
+instance JSON.ToJSON Person where
+  toJSON (Person s) = JSON.object ["name" JSON..= s]
+
+data AnswerResult = AnswerResult {
+  result :: String
+  } deriving (Show)
+instance JSON.ToJSON AnswerResult where
+  toJSON (AnswerResult s) = JSON.object ["result" JSON..= s]
+
+data AnswerError = AnswerError {
+  error :: String
+  } deriving (Show)
+instance JSON.ToJSON AnswerError where
+  toJSON (AnswerError s) = JSON.object ["error" JSON..= s]
+
+data Info = Info {
+  application :: String
+  } deriving (Show)
+instance JSON.ToJSON Info where
+  toJSON (Info s) = JSON.object ["application" JSON..= s]
+
+-- (answerQuestion $ (unpack . decodeUtf8) (maybe (pack "ma rirni la .ituk.") (\s -> s) param)
+-- (unpack . decodeUtf8) param -> String
+
 -- main = do
 --         args <- getArgs
 --         rules <- readRules <$> case args of
@@ -32,53 +83,3 @@ main :: IO ()
 
 
 -- main :: IO ()
-main = quickHttpServe site
-
-data Person  = Person {
-  name :: String
-  } deriving (Show)
-instance ToJSON Person where
-  toJSON (Person s) = object ["name" .= s]
-
-data AnswerResult = AnswerResult {
-  result :: String
-  } deriving (Show)
-instance ToJSON AnswerResult where
-  toJSON (AnswerResult s) = object ["result" .= s]
-
-data AnswerError = AnswerError {
-  error :: String
-  } deriving (Show)
-instance ToJSON AnswerError where
-  toJSON (AnswerError s) = object ["error" .= s]
-
-data Info = Info {
-  application :: String
-  } deriving (Show)
-instance ToJSON Info where
-  toJSON (Info s) = object ["application" .= s]
-
-site :: Snap ()
-site =
-    ifTop (writeJSON $ Person "me") <|>
-    route [ ("info", writeJSON $ Info "lojysamsef")
-          , ("ask", askHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
-
-answerQuestion :: String -> Maybe String
-answerQuestion q =
-  ask q $ readRules ".i la .iocikun. patfu la .ituk. .i la manam. mamta la .ituk. .i da rirni de .ijanai da patfu de .i da rirni de .ijanai da mamta de"
-
-askHandler :: Snap ()
-askHandler = do
-    param <- getParam "question"
-    maybe (writeJSON $ AnswerError "must specify echo/param in URL")
-          (\s -> (maybe
-           (writeJSON $ AnswerError "no result")
-           (\t -> (writeJSON $ AnswerResult t))
-           (answerQuestion $ (unpack . decodeUtf8) s)
-          )) param
-
--- (answerQuestion $ (unpack . decodeUtf8) (maybe (pack "ma rirni la .ituk.") (\s -> s) param)
--- (unpack . decodeUtf8) param -> String
