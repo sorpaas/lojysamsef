4dfd8a11047258668b235aaf672fd5a2c649ac19
sorpa'as plat <sorpaas@gmail.com>
2014-11-04 20:20:58 +0800

    make static ask function

--- Handler/Conversations.hs
+++ Handler/Conversations.hs
@@ -3,11 +3,17 @@
 module Handler.Conversations where
 
 import Import
+import Data.Maybe
+import Data.Text(unpack)
 import LojysambanLib(ask, readRules, loadRules, end)
 
 postAskR :: Text -> Handler Value
 postAskR ident = do
   questionValueMaybe <- lookupPostParam "question"
-  conversation <- listToMayBe $ getBy404 ConversationIdent ident
-  maybe (object ["error" .= "something wrong happened."]) returnJSONAnswer questionValueMaybe
-    where returnJSONAnswer Just q = ask q $ loadRules (conversationMessages conversation)
+--  let questionValue = fromJust questionValueMaybe in
+--    return $ object ["question" .= questionValue]
+
+--  conversation <- listToMaybe $ getBy404 ConversationIdent ident
+  let questionValue = fromJust questionValueMaybe in
+    return $ object ["answer" .= (returnJSONAnswer $ unpack questionValue)]
+      where returnJSONAnswer q = maybe "nago'i" (\s -> s) $ ask q $ loadRules [".i da du da .i la .ualeis. cu nelci lo cirla .i la gromit. cu nelci lo cirla .i la .uendolen. cu nelci lo lanme .i da pendo de .ijanai tu'e da nadu de .i da nelci di .i de nelci di"]
